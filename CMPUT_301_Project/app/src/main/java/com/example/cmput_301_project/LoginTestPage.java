2d82cfc43d535d45d0528010b792fbaabdf95e65
Ryden-Graham <rjg@ualberta.ca>
2021-10-24 11:15:38 -0600

    Testing if we can push directly to main.

--- CMPUT_301_Project/app/src/main/java/com/example/cmput_301_project/LoginTestPage.java
+++ CMPUT_301_Project/app/src/main/java/com/example/cmput_301_project/LoginTestPage.java
@@ -10,5 +10,6 @@ public class LoginTestPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_test);
+        System.out.println("Main push test");
     }
 }
