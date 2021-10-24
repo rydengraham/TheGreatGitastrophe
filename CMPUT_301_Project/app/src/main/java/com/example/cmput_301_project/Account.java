aa88433accfc30c67681fb12948bba3ccace5712
ccchoi2 <46850884+ccchoi2@users.noreply.github.com>
2021-10-24 11:35:42 -0600

    created initial data structures for accounts

---
+++ CMPUT_301_Project/app/src/main/java/com/example/cmput_301_project/Account.java
@@ -0,0 +1,44 @@
+package com.example.cmput_301_project;
+
+public class Account {
+    // Identification Information
+    private String userName;
+    private String email;
+    private String password;
+    private int id;
+
+    // Account Information
+    // TODO: Add User Habit Items
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
