a9c8a0fb443fd194ac40f0c2f266b51a405b11a5
Matheus-Du <duncanmatheus905@gmail.com>
2021-10-31 01:40:52 -0600

    added basic UI skeleton for sign-up fragment

--- CMPUT_301_Project/app/src/main/java/com/example/cmput_301_project/LoginScreenPage.java
+++ CMPUT_301_Project/app/src/main/java/com/example/cmput_301_project/LoginScreenPage.java
@@ -10,6 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -25,9 +26,22 @@ import android.widget.Toast;
 
 public class LoginScreenPage extends AppCompatActivity {
 
+    FragmentManager manager = getSupportFragmentManager();
+    FragmentTransaction transaction;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen_page);
+    }
+
+    public void onRegisterClick(View view) {
+        // create a new settings fragment and display it on the appropriate frame
+        Fragment registerFragment = new SignUpFragment();
+        //begin fragment transaction and add current activity to backstack
+        transaction = manager.beginTransaction();
+        transaction.add(R.id.signUpLayout, registerFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
