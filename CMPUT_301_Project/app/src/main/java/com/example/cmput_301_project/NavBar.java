067ad55c09c9a3bba22ad14aac62ba075875b742
hibaqw <hibaq@ualberta.ca>
2021-11-02 02:51:36 -0600

    implemented bottom navigation bar

--- CMPUT_301_Project/app/src/main/java/com/example/cmput_301_project/NavBar.java
+++ CMPUT_301_Project/app/src/main/java/com/example/cmput_301_project/NavBar.java
@@ -1,84 +1,84 @@
 package com.example.cmput_301_project;
 
+import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
 
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.sagarkoli.chetanbottomnavigation.chetanBottomNavigation;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
+/**
+ * A simple {@link Fragment} subclass.
+ * create an instance of this fragment.
+ */
 public class NavBar extends Fragment {
+
+    private String mParam1;
+    private String mParam2;
+    static final int home = 0;
+    static final int habit = 1;
+    static final int friend = 2;
+    static final int settings = 3;
+
 
     public NavBar() {
         // Required empty public constructor
     }
-    static final int home = 0;
-    static final int habit = 1;
-    static final int friends = 2;
-    static final int settings = 3;
 
-
-
-
-    @Override
+    /**
+     * @return A new instance of fragment NavBar.
+     */
+    BottomNavigationView bottomNavigationView;
+    //@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         MainPage mainActivity = (MainPage) getActivity();
-
-
         View view = inflater.inflate(R.layout.fragment_nav_bar, container, false);
-        chetanBottomNavigation  bottomNavigation = view.findViewById(R.id.navibar);
-        bottomNavigation.add(new chetanBottomNavigation.Model(home, R.mipmap.ic_home_foreground));
-        bottomNavigation.add(new chetanBottomNavigation.Model(habit, R.mipmap.ic_friend_foreground));
-        bottomNavigation.add(new chetanBottomNavigation.Model(friends, R.mipmap.ic_habit_foreground));
-        bottomNavigation.add(new chetanBottomNavigation.Model(settings, R.mipmap.ic_settings_foreground));
-        bottomNavigation.setOnShowListener(new chetanBottomNavigation.ShowListener(){
+        bottomNavigationView = view.findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            String name;
+            switch (item.getItemId()) {
+                case R.id.home:
+                    name = "HOME";
+                    mainActivity.textView.setText(name);
+                    break;
 
-            @Override
-            public void onShowItem(chetanBottomNavigation.Model item) {
-                String name;
-                switch (item.getId()) {
-                    case home:
-                        name = "HOME";
-                        mainActivity.textView.setText(name);
-                        break;
-                    case habit:
-                        name = "FRIENDS";
-                        mainActivity.textView.setText(name);
-                        break;
-                    case friends:
-                        name = "HABITS";
-                        mainActivity.textView.setText(name);
-                        break;
-                    case settings:
-                        name = "SETTINGS";
-                        mainActivity.textView.setText(name);
-                        break;
-                    default:
-                        name = "";
-                }
+                case R.id.habits:
+                    name = "HABITS";
+                    mainActivity.textView.setText(name);
+                    break;
+
+                case R.id.friends:
+                    name = "FRIENDS";
+                    mainActivity.textView.setText(name);
+                    break;
+
+                case R.id.settings:
+                    name = "SETTINGS";
+                    mainActivity.textView.setText(name);
+                    break;
+
+                default:
+                    name = "";
             }
+            return true;
+
         });
-
-        bottomNavigation.setOnClickMenuListener(new chetanBottomNavigation.ClickListener() {
-            @Override
-            public void onClickItem(chetanBottomNavigation.Model item) {
-                Toast.makeText(view.getContext(), "Item clicked", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-        bottomNavigation.show(home,true);
-
 
 
         return view;
 
+
     }
 
 }
+
+
