a519e71881188ee7902ff5f7f9a0b065f20b5ab3
Ryden-Graham <rjg@ualberta.ca>
2021-11-05 14:41:03 -0600

    removed dialogue box so tests don't break.

--- CMPUT_301_Project/app/src/main/java/com/example/cmput_301_project/UserProfilePage.java
+++ CMPUT_301_Project/app/src/main/java/com/example/cmput_301_project/UserProfilePage.java
@@ -7,7 +7,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -60,16 +59,6 @@ public class UserProfilePage extends AppCompatActivity {
 
         habitList.setAdapter(habitAdapter);
 
-        // TODO: Integrate events
-        // Standard TBD Alert Dialogue
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setCancelable(true);
-        builder.setTitle("This Is Not Connected Yet");
-        builder.setMessage("Habit integration will be added in project part 4. Existing events are for GUI testing only. The gear icon in the top right takes you to the settings page");
-        builder.setNegativeButton("OK", null);
-        // create the alert dialog and display it over the fragment
-        AlertDialog alertBox = builder.create();
-        alertBox.show();
     }
 
     public void onSettingsClick(View view) {
