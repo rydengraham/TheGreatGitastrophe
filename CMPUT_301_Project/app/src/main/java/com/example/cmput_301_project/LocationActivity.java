fdc2b6aa3f6a248327b295543bcc250466201bc4
hibaqw <hibaq@ualberta.ca>
2021-11-26 06:25:49 -0700

    Added LocationActivity, and HabitLocation classes

--- CMPUT_301_Project/app/src/main/java/com/example/cmput_301_project/LocationActivity.java
+++ CMPUT_301_Project/app/src/main/java/com/example/cmput_301_project/LocationActivity.java
@@ -1,15 +1,217 @@
 package com.example.cmput_301_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class LocationActivity extends AppCompatActivity {
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class LocationActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    // Constants
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    private static final String TRACKING_LOCATION_KEY = "tracking_location";
+    private static final String TAG = LocationActivity.class.getSimpleName();
+
+
+    HabitLocation locationDetail;
+    String mLongitude;
+    String mAddress;
+    String mLatitude;
+    LatLng latLng;
+
+    // Location classes
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Location mLastLocation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+        initializeHabitLocation();
+
+        String apiKey = getString(R.string.api_key);
+
+        if(!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), apiKey);
+
+        }
+        PlacesClient placesClient = Places.createClient(this);
+
+
+        // Initialize the AutocompleteSupportFragment.
+
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG, Place.Field.ADDRESS));
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+                setUpMap(place.getLatLng());
+            }
+
+
+            @Override
+            public void onError(@NonNull Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSION:
+                // If the permission is granted, get the location,
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLocation();
+                } else {
+                    Toast.makeText(this,
+                            R.string.location_permission_denied,
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    private void initializeHabitLocation() {
+        getLocation();
+        HabitLocation locationDetail= new HabitLocation(mAddress,mLongitude,mLatitude);
+
+    }
+
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                            {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        } else {
+            Log.d(TAG, "getLocation: permissions granted");
+        }
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    mLastLocation = location;
+                    Toast.makeText(getApplicationContext(), mLastLocation.getLatitude() + "," +
+                            mLastLocation.getLongitude(), Toast.LENGTH_LONG).show();
+
+                    setUpMap(new LatLng(mLastLocation.getLatitude(),mLastLocation.getLongitude()));
+
+
+
+                    mLatitude = Double.toString(location.getLatitude());
+                    mLongitude = Double.toString(location.getLongitude());
+
+
+
+                    setAddress(location);
+                } else {
+                    Toast.makeText(LocationActivity.this,
+                            R.string.location_permission_denied,
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void setUpMap(LatLng newLatLng){
+        latLng = newLatLng;
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(LocationActivity.this::onMapReady);
+
+    }
+
+    private void setAddress(Location location) {
+        Geocoder geocoder = new Geocoder(LocationActivity.this,
+                Locale.getDefault());
+        List<Address> addresses = null;
+        String resultMessage = "";
+
+        try {
+            addresses = geocoder.getFromLocation(
+                    location.getLatitude(),
+                    location.getLongitude(),
+                    // In this sample, get just a single address
+                    1);
+        } catch (IOException ioException) {
+            // Catch network or other I/O problems
+            resultMessage = LocationActivity.this
+                    .getString(R.string.service_not_available);
+            Log.e(TAG, resultMessage, ioException);
+        }
+
+        if (addresses == null || addresses.size() == 0) {
+            if (resultMessage.isEmpty()) {
+                resultMessage = LocationActivity.this
+                        .getString(R.string.no_address_found);
+                Log.e(TAG, resultMessage);
+            }
+        } else {
+            Address address = addresses.get(0);
+            StringBuilder out = new StringBuilder();
+            // Fetch the address lines using getAddressLine,
+            // join them, and send them to the thread
+            for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+                out.append(address.getAddressLine(i));
+            }
+
+            resultMessage = out.toString();
+        }
+        mAddress = resultMessage;
+    }
+
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        MarkerOptions markerOptions=new MarkerOptions().position(latLng).title("My Location");
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+        googleMap.addMarker(markerOptions);
 
     }
 }
