3f95784a5aa81ae3bada31958f7de045c5adb9eb
hibaqw <hibaq@ualberta.ca>
2021-11-26 06:01:49 -0700

    Setting up LocatonActivity: added api_key, strings.xml..

---
+++ CMPUT_301_Project/app/src/main/java/com/example/cmput_301_project/LocationActivity.java
@@ -0,0 +1,15 @@
+package com.example.cmput_301_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+
+    }
+}
