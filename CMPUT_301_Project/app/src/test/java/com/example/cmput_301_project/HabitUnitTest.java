d6e5e4a8f24aa7cc57d618809eed14f0a719e3e8
Yazan Al-Muhtaseb <yalmuhta@ualberta.ca>
2021-11-28 15:58:50 -0700

    Unit test code tidying

--- CMPUT_301_Project/app/src/test/java/com/example/cmput_301_project/HabitUnitTest.java
+++ CMPUT_301_Project/app/src/test/java/com/example/cmput_301_project/HabitUnitTest.java
@@ -14,32 +14,27 @@ import static org.junit.Assert.*;
 public class HabitUnitTest {
     @Test
     public void CreateHabit() {
+        
         Habit testHabit = new Habit("TestHabit", new Date(2017,11,12), "For testing uses", 64, true);
-
         assertEquals(testHabit.getHabitName(), "TestHabit");
     }
 
     @Test
     public void ChangeAttributes() {
+
         Habit testHabit = new Habit("TestHabit", new Date(2017-1900,11,12), "For testing uses", 64, true);
-
         assertEquals(testHabit.getHabitName(), "TestHabit");
-
         testHabit.setHabitName("TestHabitRename");
         assertNotEquals(testHabit.getHabitName(), "TestHabit");
         assertEquals(testHabit.getHabitName(), "TestHabitRename");
-
         testHabit.setPublic(false);
         assertEquals(testHabit.getPublic(), false);
-
         testHabit.setReason("To change to something else");
         assertNotEquals(testHabit.getReason(), "For testing uses");
         assertEquals(testHabit.getReason(), "To change to something else");
-
         testHabit.setStartDate( new Date(2018-1900,11,12));
         assertNotEquals(testHabit.getStartDate(), new Date(2017-1900,11,12));
         assertEquals(testHabit.getStartDate(), new Date(2018-1900,11,12));
-
         testHabit.setWeekdays(1);
         assertNotEquals(testHabit.getWeekdays(), 64);
         assertEquals(testHabit.getWeekdays(), 1);
@@ -48,16 +43,12 @@ public class HabitUnitTest {
 
     @Test
     public void CreateHabitEvents() {
+
         Habit testHabit = new Habit("TestHabit", new Date(2017-1900,11,12), "For testing uses", 64, true);
-
         assertEquals(testHabit.getHabitName(), "TestHabit");
-
         testHabit.addHabitEvent(new HabitEvent("2000/11/12","TestEventHabit"));
-
         assertEquals(testHabit.getHabitEventTable().size(), 1);
-
         assertEquals(testHabit.getHabitEventTable().get(0).getTitle(), "TestEventHabit");
-
         testHabit.getHabitEventTable().remove(testHabit.getHabitEventTable().get(0));
         assertEquals(testHabit.getHabitEventTable().size(), 0);
 
