aba89a9b2bcd4fa36822df557d0235a9d89e5e60
hibaqw <hibaq@ualberta.ca>
2021-11-28 07:27:10 -0700

    implemented basic camera functionality

---
+++ CMPUT_301_Project/app/src/main/java/com/example/cmput_301_project/CameraActivity.java
@@ -0,0 +1,96 @@
+package com.example.cmput_301_project;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class CameraActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CAMERA_PERMISSION = 2;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final String TAG = CameraActivity.class.getSimpleName();
+    Button btnCamera;
+    Button btnGallery;
+    ImageView habitImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ;
+        setContentView(R.layout.activity_camera);
+        btnCamera = findViewById(R.id.camera_btn);
+        btnGallery = findViewById(R.id.gallery_btn);
+        habitImage = findViewById(R.id.habit_image);
+        setCameraPermission();
+
+
+    }
+
+    public void setCameraPermission() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                            {Manifest.permission.CAMERA},
+                    REQUEST_CAMERA_PERMISSION);
+        } else {
+            takePicture();
+            Log.d(TAG, "Camera: permissions granted");
+        }
+
+    }
+
+    public void takePicture() {
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CAMERA_PERMISSION:
+                // If the permission is granted, proceed to access camera,
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    Toast.makeText(this,
+                            "Unable to use Camera",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+        }
+    }
+}
