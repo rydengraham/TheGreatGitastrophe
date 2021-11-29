593980ae3a1084932a452bb2845e6be1b60da757
hibaqw <hibaq@ualberta.ca>
2021-11-29 12:17:10 -0700

    added camera functionality

---
+++ CMPUT_301_Project/app/src/main/java/com/example/cmput_301_project/CameraActivity.java
@@ -0,0 +1,177 @@
+package com.example.cmput_301_project;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraX;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageAnalysisConfig;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureConfig;
+import androidx.camera.core.Preview;
+import androidx.camera.core.PreviewConfig;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Matrix;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Log;
+import android.util.Rational;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.io.File;
+
+/**
+ * Code sourced from: https://github.com/akindofyoga/CameraXJava
+ */
+public class CameraActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CAMERA_PERMISSION = 2;
+    private static final String TAG =CameraActivity.class.getSimpleName() ;
+    TextureView viewFinder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        viewFinder = findViewById(R.id.view_finder);
+        getCameraPermissions();
+    }
+    public void getCameraPermissions() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                            {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CAMERA_PERMISSION);
+
+        } else {
+            Log.d(TAG, "CAMERA: permissions granted");
+            viewFinder.post(startCamera);
+
+        }
+    }
+    private Runnable startCamera = new Runnable() {
+        @Override
+        public void run() {
+            // Create configuration object for the viewfinder use case
+            PreviewConfig previewConfig = new PreviewConfig.Builder()
+                    .setTargetAspectRatio(new Rational(1, 1))
+                    .setTargetResolution(new Size(640, 640))
+                    .build();
+
+            // Build the viewfinder use case
+            Preview preview = new Preview(previewConfig);
+
+            // Every time the viewfinder is updated, recompute layout
+            preview.setOnPreviewOutputUpdateListener(
+                    previewOutput -> {
+                        // To update the SurfaceTexture, we have to remove it and re-add it
+                        ViewGroup parent = (ViewGroup) viewFinder.getParent();
+                        parent.removeView(viewFinder);
+                        parent.addView(viewFinder, 0);
+
+                        viewFinder.setSurfaceTexture(previewOutput.getSurfaceTexture());
+                        updateTransform();
+                    });
+
+            // Create configuration object for the image capture use case
+            ImageCaptureConfig imageCaptureConfig = new ImageCaptureConfig.Builder()
+                    .setTargetAspectRatio(new Rational(1, 1))
+                    // We don't set a resolution for image capture; instead, we
+                    // select a capture mode which will infer the appropriate
+                    // resolution based on aspect ration and requested mode
+                    .setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+                    .build();
+
+            // Build the image capture use case and attach button click listener
+            ImageCapture imageCapture = new ImageCapture(imageCaptureConfig);
+            findViewById(R.id.capture_button).setOnClickListener(view -> {
+                File file = new File(getExternalMediaDirs()[0], System.currentTimeMillis() + ".jpg");
+                imageCapture.takePicture(file, new ImageCapture.OnImageSavedListener(){
+                    @Override
+                    public void onError(ImageCapture.UseCaseError error, String message,
+                                        @Nullable Throwable exc) {
+                        String msg = "Photo capture failed: " + message;
+                        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
+                        Log.e(TAG, msg);
+                        if (exc != null) {
+                            exc.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onImageSaved(File file) {
+                        String msg = "Photo capture succeeded: " + file.getAbsolutePath();
+                        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, msg);
+                    }
+                });
+
+            });
+
+
+            CameraX.bindToLifecycle(CameraActivity.this, preview, imageCapture);
+        }
+    };
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CAMERA_PERMISSION:
+                // If the permission is granted, get the location,
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    viewFinder.post(startCamera);
+                } else {
+                    Toast.makeText(this,
+                            "Unable to Access Camera",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+                break;
+        }
+    }
+
+    private void updateTransform() {
+        Matrix matrix = new Matrix();
+
+        float centerX = viewFinder.getWidth() / 2f;
+        float centerY = viewFinder.getHeight() / 2f;
+
+        // Correct preview output to account for display rotation
+        float rotationDegrees;
+        switch (viewFinder.getDisplay().getRotation()) {
+            case Surface.ROTATION_0:
+                rotationDegrees = 0f;
+                break;
+            case Surface.ROTATION_90:
+                rotationDegrees = 90f;
+                break;
+            case Surface.ROTATION_180:
+                rotationDegrees = 180f;
+                break;
+            case Surface.ROTATION_270:
+                rotationDegrees = 270f;
+                break;
+            default:
+                return;
+        }
+
+        matrix.postRotate(-rotationDegrees, centerX, centerY);
+
+        // Finally, apply transformations to our TextureView
+        viewFinder.setTransform(matrix);
+    }
+
+
+}
