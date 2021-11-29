fb6402fcf6e7d06c4d781508c9953a0a77eac2de
hibaqw <hibaq@ualberta.ca>
2021-11-29 12:25:08 -0700

    left comments on location activity

--- CMPUT_301_Project/app/src/main/java/com/example/cmput_301_project/LocationActivity.java
+++ CMPUT_301_Project/app/src/main/java/com/example/cmput_301_project/LocationActivity.java
@@ -38,6 +38,10 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 
+/**
+ * Code Sourced in part from: https://github.com/dimitardanailov/FusedLocationProviderClient/blob/master/app/src/main/java/demo/client/provider/location/fused/fusedlocationproviderclientexample/MainActivity.java
+ */
+
 public class LocationActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     // Constants
